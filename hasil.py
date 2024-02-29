a640e09549170105e23920b8e45268e1f01edb1a
evadevia09 <160815280+evadevia09@users.noreply.github.com>
2024-02-29 11:24:21 +0700

    Add files via upload

---
+++ hasil.py
@@ -0,0 +1,12 @@
+# ucup membeli barang ke warung 
+
+rokok = 20.000
+minyak = 25.000
+gula = 11.000
+saldo = 100.000
+Diskon = 10.000
+
+kembalian =(saldo-(rokok+minyak+gula-Diskon))
+
+print('uang kembalian ucup :',kembalian)
+
